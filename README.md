fefbf68f94902bbd40e5448201b2c3f19a4e32fe
System Administrator <1357926740@qq.com>
2023-11-28 01:42:46 +0800

    update repo

---
+++ README.md
@@ -0,0 +1,6 @@
+#### 🚗🚗🚗
+
+#### ❤️刀刀源 https://xiangfeidexiaohuo.github.io/
+
+#### 支持rootfull和rootless、roothide，主要是一些汉化包和插件。
+
--- gen.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-find ../ -name ".DS_Store" -depth -exec rm {} \;
-#dpkg-scanpackages -m ./debs > ./Packages
-#bzip2 -fks ./Packages
-
-dpkg-scanpackages -m ./debs /dev/null > ./Packages
-tar zcvf ./Packages.gz ./Packages
-bzip2 -fks ./Packages
